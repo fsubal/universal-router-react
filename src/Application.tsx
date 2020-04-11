f85904c7d18bf6a0099e5652139230b1b858568e
fsubal <fsubal1102@gmail.com>
2020-04-11 20:13:18 +0900

    write wip

---
+++ src/Application.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import UniversalRouter from "universal-router";
+import NonSpaRoute from "./NonSpaRoute";
+import ApplicationContext from "./ApplicationContext";
+
+export const NavLinkContext = React.createContext(null as any);
+
+interface Props<C> {
+  context: C;
+  router: UniversalRouter;
+  children: React.ReactNode;
+}
+
+export default function Application<C>({
+  router,
+  context,
+  children,
+}: Props<C>) {
+  const [currentChildren, setCurrentChildren] = useState(children);
+
+  const navigate = async (path: string) => {
+    const url = new URL(path);
+    const next = await router.resolve(url.pathname);
+    if (!React.isValidElement(next)) {
+      throw new Error("not a valid route");
+    }
+
+    if (next.type === NonSpaRoute) {
+      location.href = url.href;
+      return;
+    }
+
+    setCurrentChildren(next);
+    // history.pushState()
+  };
+
+  return (
+    <ApplicationContext.Provider value={context}>
+      <NavLinkContext.Provider value={navigate}>
+        <>{currentChildren}</>
+      </NavLinkContext.Provider>
+    </ApplicationContext.Provider>
+  );
+}
